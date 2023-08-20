a883eaf2b659d5e50cac2f984fef4cc322fd081c
ali-mashayekhi <alimashayekhi42@gmail.com>
2023-08-20 12:13:40 +0330

    new template added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { useState } from "react";
-
-import InputForm from "./components/InputForm/InputForm";
-import UsersList from "./components/UsersList/UsersList";
-
-const storedUsers = [
-  { username: "Alimasha", age: "24", id: "1321" },
-  { username: "bladeOfMercy", age: "24", id: "3214123" },
-];
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [users, setUsers] = useState(storedUsers);
-
-  function addUserHandler(userData) {
-    setUsers((previousState) => [userData, ...previousState]);
-    storedUsers.unshift(userData);
-  }
-
   return (
-    <div>
-      <InputForm onAddUser={addUserHandler} />
-      <UsersList users={users} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
